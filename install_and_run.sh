3f8f3ea5fbfba45ee4490d499d6dfb8ec872ddcd
shwepz <shwepz@localhost.localdomain>
2024-08-10 02:10:17 +0700

    added venv

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Setting up virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 echo "Installing required packages..."
 pip install -r requirements.txt
 
-echo "Running NektoPSY bot..."
+echo "Running NektoSPY bot..."
 python main.py
 
 read -p "Press any key to continue..."
